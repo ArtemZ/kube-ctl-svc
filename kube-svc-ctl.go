a165fb4951a9befb90a1683a6a248b24d7a92a68
Artem Zhirkov <artem@flotechnologies.com>
2021-02-01 20:57:04 +0300

    Implement retry mechanism

--- kube-svc-ctl.go
+++ kube-svc-ctl.go
@@ -5,6 +5,7 @@ import (
 	"kube-svc-ctl/k8s/secret"
 	"kube-svc-ctl/svc"
 	"kube-svc-ctl/vault"
+	"net"
 )
 import "flag"
 import "os"
@@ -15,8 +16,17 @@ func validateKubernetesResource(name *string, namespace *string, resourceType *s
 }
 func generateSvcConfig(name *string, tag *string, credentials *vault.VaultCredentials) string {
 	c := vault.NewVaultAuthenticatedClient(credentials)
+	var reties = 10
 	r, err := vault.ReadVaultSecret(c, "secret/data/service/"+*name)
-	if err != nil {
+	for reties != 0 {
+		if test, ok := err.(net.Error); ok && test.Timeout() {
+			r, err = vault.ReadVaultSecret(c, "secret/data/service/"+*name)
+			reties = reties - 1
+		} else {
+			panic(err)
+		}
+	}
+	if reties == 0 && err != nil {
 		panic(err)
 	}
 
