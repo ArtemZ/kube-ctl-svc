11a8dba061dc8eb3050c5b826c3052d191bf81e5
Artem Zhirkov <Artem.Zhirkov@fbwinn.com>
2023-07-21 14:22:27 -0400

    - Refactor command execution and structure of the tool - Add `-target-yaml-tree` flag

--- kube-svc-ctl.go
+++ kube-svc-ctl.go
@@ -1,179 +1,37 @@
 package main
 
 import (
-	"kube-svc-ctl/docker"
-	"kube-svc-ctl/k8s/secret"
-	"kube-svc-ctl/svc"
-	"kube-svc-ctl/vault"
-	"net"
+	"kube-svc-ctl/cmd/actions"
 )
 import "flag"
 import "os"
 import "fmt"
 
-func validateKubernetesResource(name *string, namespace *string, resourceType *string) bool {
-	return false
-}
-func generateSvcConfig(name *string, tag *string, credentials *vault.VaultCredentials) string {
-	c := vault.NewVaultAuthenticatedClient(credentials)
-	var retries = 10
-	r, err := vault.ReadVaultSecret(c, "secret/data/service/"+*name)
-	for retries != 0 && err != nil {
-		if test, ok := err.(net.Error); ok && test.Timeout() {
-			r, err = vault.ReadVaultSecret(c, "secret/data/service/"+*name)
-			retries = retries - 1
-		} else {
-			panic(err)
-		}
-	}
-	if retries == 0 && err != nil {
-		panic(err)
-	}
-
-	s := svc.NewHelmConfig(tag, &r)
-	return s.ToYaml()
-}
-
-func generateRegistrySecretManifest(credentials *vault.VaultCredentials) {
-	c := vault.NewVaultAuthenticatedClient(credentials)
-	d, err := vault.ReadVaultSecret(c, "secret/data/common/registry")
-	var r map[string]interface{}
-	if subdata, ok := d["data"]; ok {
-		r = subdata.(map[string]interface{})
-	} else {
-		r = d
-	}
-	if err != nil {
-		panic(err)
-	}
-	var registryUsername string
-	var registryPassword string
-	var registryAddr string
-
-	if p, ok := r["registry_password"].(string); ok {
-		registryPassword = p
-	} else {
-		panic("Registry password retrieved from Vault is not valid")
-	}
-	if p, ok := r["registry_username"].(string); ok {
-		registryUsername = p
-	} else {
-		registryUsername = "k8s-registry-token"
-	}
-	if p, ok := r["registry_addr"].(string); ok {
-		registryAddr = p
-	} else {
-		registryAddr = "registry.gitlab.com"
-	}
-	creds := docker.RegistryCredentials{
-		Addr:     registryAddr,
-		Username: registryUsername,
-		Password: registryPassword,
-	}
-	m := secret.NewSecret(
-		"gitlab-registry",
-		"kubernetes.io/dockerconfigjson",
-		creds.EncryptedDockerconfigjson())
-	fmt.Println(m.YamlManifest())
-}
-
-func addVaultFlags(set *flag.FlagSet) {
-	set.String(
-		"vault-url",
-		"http://localhost:8200",
-		"Hashicorp Vault Url. Can be overridden by VAULT_URL environment variable. Default: http://localhost:8200",
-	)
-	set.String(
-		"vault-token",
-		"",
-		"Hashicorp Vault Access Token. Can be overridden by VAULT_TOKEN environment variable (RECOMMENDED)",
-	)
-}
-
-func validateVaultFlags(url *string, token *string) *vault.VaultCredentials {
-	url_env, vaultUrlOk := os.LookupEnv("VAULT_URL")
-	token_env, vaultTokenOk := os.LookupEnv("VAULT_TOKEN")
-	if (*url == "" || *token == "") && (!vaultUrlOk || !vaultTokenOk) {
-		return nil
-	}
-	if !vaultUrlOk || !vaultTokenOk {
-		creds := vault.VaultCredentials{
-			Url:   *url,
-			Token: *token,
-		}
-		return &creds
-	}
-	creds := vault.VaultCredentials{
-		Url:   url_env,
-		Token: token_env,
-	}
-	return &creds
-}
-
 func main() {
-	validateCommand := flag.NewFlagSet("validate", flag.ExitOnError)
-	validateCommandService := validateCommand.String("service", "", "Service name (Required)")
-	validateCommandResource := validateCommand.String("resource", "", "Resource type <secret|> (Required)")
-	validateCommandResourceName := validateCommand.String("resource-name", "", "Resource name to validate (Required)")
-
-	generateConfigCommand := flag.NewFlagSet("generate-svc-config", flag.ExitOnError)
-	generateConfigService := generateConfigCommand.
-		String("service", "", "Service name")
-	generateConfigCommand.Bool("add-docker-image-tag", true, "Add information about service's docker tag to generated configuration. Default: true")
-	generateConfigTag := generateConfigCommand.String("tag", "latest", "Service's docker image tag. Default: latest")
-	addVaultFlags(generateConfigCommand)
-
-	generateSecretManifest := flag.NewFlagSet("generate-secret-manifest", flag.ExitOnError)
-	addVaultFlags(generateSecretManifest)
-
 	if len(os.Args) < 2 {
-		fmt.Println("One of the following commands is required: validate|generate-svc-config|generate-secret-manifest")
+		fmt.Println("One of the following commands is required: validate|generate-helm-values|generate-secret-manifest")
 	}
 	switch os.Args[1] {
 	case "validate":
-		_ = validateCommand.Parse(os.Args[2:])
+		actions.ValidateCommand()
 	case "generate-svc-config":
-		_ = generateConfigCommand.Parse(os.Args[2:])
+		err := actions.GenerateHelmValues()
+		if err != nil {
+			fmt.Println(err)
+		}
+	case "generate-helm-values":
+		err := actions.GenerateHelmValues()
+		if err != nil {
+			fmt.Println(err)
+		}
 	case "generate-secret-manifest":
-		_ = generateSecretManifest.Parse(os.Args[2:])
+		err := actions.GenerateSecretManifest()
+		if err != nil {
+			fmt.Println(err)
+		}
 	default:
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	if validateCommand.Parsed() {
-		// Check required flags
-		if *validateCommandService == "" || *validateCommandResource == "" {
-			validateCommand.PrintDefaults()
-			os.Exit(1)
-		}
-		validateKubernetesResource(validateCommandResourceName, validateCommandService, validateCommandResource)
-	}
-	if generateConfigCommand.Parsed() {
-		if *generateConfigService == "" {
-			generateConfigCommand.PrintDefaults()
-			os.Exit(1)
-		}
-		u := generateConfigCommand.Lookup("vault-url").Value.String()
-		t := generateConfigCommand.Lookup("vault-token").Value.String()
-		addTag := generateConfigCommand.Lookup("add-docker-image-tag").Value.String()
-		creds := validateVaultFlags(&u, &t)
-		if creds == nil {
-			fmt.Println("Vault flags are invalid")
-			os.Exit(1)
-		}
-		if addTag == "false" {
-			generateConfigTag = nil
-		}
-		fmt.Println(generateSvcConfig(generateConfigService, generateConfigTag, creds))
-	}
-	if generateSecretManifest.Parsed() {
-		u := generateSecretManifest.Lookup("vault-url").Value.String()
-		t := generateSecretManifest.Lookup("vault-token").Value.String()
-		creds := validateVaultFlags(&u, &t)
-		if creds == nil {
-			fmt.Println("Vault flags are invalid")
-			os.Exit(1)
-		}
-		generateRegistrySecretManifest(creds)
-	}
+
 }
--- svc/svc.go
+++ svc/svc.go
@@ -3,19 +3,53 @@ package svc
 import (
 	"gopkg.in/yaml.v2"
 	"log"
+	"strings"
 )
 
 type HelmConfig struct {
-	Image   DockerImageHelmConfig
-	Secrets SecretsHelmConfig
+	tag        *string
+	secrets    *map[string]interface{}
+	targetTree *string
 }
 
-type DockerImageHelmConfig struct {
-	Tag string
-}
+func (s *HelmConfig) ToYaml() string {
+	tree := make(map[string]interface{})
 
-type SecretsHelmConfig struct {
-	Datas map[string]interface{}
+	if s.targetTree != nil {
+		// makes map[value][value][value] out of
+		// "value.value.value" string
+		var lastBranch *map[string]interface{} = nil
+		splitTargetTree := strings.Split(*s.targetTree, ".")
+		for index, elem := range splitTargetTree {
+			subtree := make(map[string]interface{})
+			if lastBranch == nil {
+				tree[elem] = subtree
+			} else {
+				(*lastBranch)[elem] = subtree
+			}
+			if len(splitTargetTree) == index+1 {
+				(*lastBranch)[elem] = s.secrets
+			} else {
+				lastBranch = &subtree
+			}
+		}
+
+	} else {
+		tree = *s.secrets
+	}
+
+	if s.tag != nil {
+		tree["image"] = map[string]map[string]string{}
+		t := make(map[string]interface{})
+		t["tag"] = s.tag
+		tree["image"] = t
+	}
+
+	y, err := yaml.Marshal(tree)
+	if err != nil {
+		log.Fatalf("Yaml Marshalling error: %v", err)
+	}
+	return string(y)
 }
 
 func mapIndexExists(m *map[string]interface{}, i string) bool {
@@ -23,15 +57,7 @@ func mapIndexExists(m *map[string]interface{}, i string) bool {
 	return exists
 }
 
-func (s *HelmConfig) ToYaml() string {
-	y, err := yaml.Marshal(s)
-	if err != nil {
-		log.Fatalf("Yaml Marshalling error: %v", err)
-	}
-	return string(y)
-}
-
-func NewHelmConfig(dockerImageTag *string, secrets *map[string]interface{}) HelmConfig {
+func NewHelmConfig(dockerImageTag *string, secrets *map[string]interface{}, targetTree *string) HelmConfig {
 	var s map[string]interface{}
 	// check that we are working with a newer Vault version
 	// retrieve values from "data" submap in this case
@@ -40,23 +66,11 @@ func NewHelmConfig(dockerImageTag *string, secrets *map[string]interface{}) Helm
 	} else { // use returned values directly otherwise
 		s = *secrets
 	}
-	sHelmConfig := SecretsHelmConfig{
-		Datas: s,
+	sHelmConfig := HelmConfig{
+		secrets:    &s,
+		tag:        dockerImageTag,
+		targetTree: targetTree,
 	}
-	var h HelmConfig
-	if dockerImageTag != nil {
-		i := DockerImageHelmConfig{
-			Tag: *dockerImageTag,
-		}
-		h = HelmConfig{
-			Image:   i,
-			Secrets: sHelmConfig,
-		}
-		return h
-	}
-	h = HelmConfig{
-		Secrets: sHelmConfig,
-	}
-	return h
+	return sHelmConfig
 
 }
